2af208283c4f41b49169d5dfbba81f59d07add09
travelbeeee <sochun1518@naver.com>
2021-05-21 15:28:40 +0900

    Member가 가지고 있는 Item 조회기능 추가

--- V2.0/PDFLO_V2.0/src/main/java/travelbeeee/PDFLO_V20/repository/ItemRepository.java
+++ V2.0/PDFLO_V2.0/src/main/java/travelbeeee/PDFLO_V20/repository/ItemRepository.java
@@ -1,6 +1,7 @@
 package travelbeeee.PDFLO_V20.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import travelbeeee.PDFLO_V20.domain.entity.Item;
 import travelbeeee.PDFLO_V20.domain.entity.Member;
@@ -8,5 +9,6 @@ import travelbeeee.PDFLO_V20.domain.entity.Member;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    List<Item> findByMember(Member member);
+    @Query("select i from Item i where i.member.id = :memberId")
+    List<Item> findByMember(@Param("memberId") Long memberId);
 }
