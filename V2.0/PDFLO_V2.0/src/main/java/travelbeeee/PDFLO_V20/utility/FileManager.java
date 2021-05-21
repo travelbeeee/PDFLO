1c897b35eb958a602f8efc1c1cb050287008c8fd
travelbeeee <sochun1518@naver.com>
2021-05-21 14:58:49 +0900

    FileManager 개발!! --> 서비스 단에서의 File 관련 권한을 FileManager에게 다 권한위임

--- V2.0/PDFLO_V2.0/src/main/java/travelbeeee/PDFLO_V20/utility/FileManager.java
+++ V2.0/PDFLO_V2.0/src/main/java/travelbeeee/PDFLO_V20/utility/FileManager.java
@@ -1,16 +1,54 @@
 package travelbeeee.PDFLO_V20.utility;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.jni.FileInfo;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import travelbeeee.PDFLO_V20.domain.FileInformation;
+import travelbeeee.PDFLO_V20.domain.enumType.FileType;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 
 @Service
+@RequiredArgsConstructor
 public class FileManager {
+
+    @Value("${file.location}")
+    private String rootLocation;
+
+    private HashMap<FileType,String> locationMap = new HashMap<FileType,String>();
+    private final Sha256Encryption sha256Encryption;
+
+    @PostConstruct
+    private void postConstruct(){
+        locationMap.put(FileType.PDF, "/PDF");
+        locationMap.put(FileType.PROFILE, "/PROFILE");
+        locationMap.put(FileType.THUMBNAIL, "/THUMBNAIL");
+    }
+
+    public FileInformation fileUpload(MultipartFile file, FileType fileType) throws NoSuchAlgorithmException {
+        String fileName = sha256Encryption.sha256(file.getOriginalFilename(), sha256Encryption.makeSalt());
+        String extension = file.getOriginalFilename().substring(file.getOriginalFilename().indexOf("."));
+        String location = rootLocation + locationMap.get(fileType);
+
+        try{
+            Files.copy(file.getInputStream(), Paths.get(location + "/" + fileName + extension), StandardCopyOption.REPLACE_EXISTING);
+        }catch(IOException e){
+            return null;
+        }
+        return new FileInformation(fileName, location, extension);
+    }
 
     public boolean fileUpload(InputStream inputStream, String location, String fileName) {
         try{
