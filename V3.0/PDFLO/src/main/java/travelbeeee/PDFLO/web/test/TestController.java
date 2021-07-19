7942df933aca719bd03264b10e1218f15caa45cb
travelbeeee <sochun1518@naver.com>
2021-07-19 17:22:52 +0900

    Ajax 통신 테스트

--- V3.0/PDFLO/src/main/java/travelbeeee/PDFLO/web/test/TestController.java
+++ V3.0/PDFLO/src/main/java/travelbeeee/PDFLO/web/test/TestController.java
@@ -1,12 +1,13 @@
 package travelbeeee.PDFLO.web.test;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Slf4j
 @Controller
@@ -15,13 +16,19 @@ public class TestController {
     @GetMapping("/test")
     public String testForm(Model model) {
         model.addAttribute("testForm", new TestForm());
+        log.info("GET test");
         return "/test/test";
     }
 
-    @ResponseBody
     @PostMapping("/test")
-    public TestForm test(@ModelAttribute TestForm testForm) {
-        log.info("testForm : {}", testForm);
-        return testForm;
+    public String test() {
+        log.info("POST test");
+        return "redirect:/";
+    }
+
+    @PostMapping("/test2")
+    public ResponseEntity<?> test(@RequestParam String email) throws IOException {
+        log.info("email: " + email);
+        return ResponseEntity.ok().body("Hello");
     }
 }
--- V3.0/PDFLO/src/main/java/travelbeeee/PDFLO/web/test/TestForm.java
+++ V3.0/PDFLO/src/main/java/travelbeeee/PDFLO/web/test/TestForm.java
@@ -2,8 +2,9 @@ package travelbeeee.PDFLO.web.test;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-@Getter @Setter
+@Getter @Setter @ToString
 public class TestForm {
     private String email;
 }
