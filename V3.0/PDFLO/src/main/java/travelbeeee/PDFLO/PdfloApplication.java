5d59d53ab379d91b355c29819a04b81f3f73b326
travelbeeee <sochun1518@naver.com>
2021-07-19 12:45:10 +0900

    지이이인짜최종버전git add .! PDFLO V3.0

---
+++ V3.0/PDFLO/src/main/java/travelbeeee/PDFLO/PdfloApplication.java
@@ -0,0 +1,13 @@
+package travelbeeee.PDFLO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfloApplication.class, args);
+	}
+
+}
