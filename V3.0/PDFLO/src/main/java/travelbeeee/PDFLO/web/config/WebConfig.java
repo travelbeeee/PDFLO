f17fe6dd43de14db30a5764f7c76fe5dd40a782e
travelbeeee <sochun1518@naver.com>
2021-07-22 23:51:55 +0900

    Intercepter 기능 개발동안 잠깐 끄기

--- V3.0/PDFLO/src/main/java/travelbeeee/PDFLO/web/config/WebConfig.java
+++ V3.0/PDFLO/src/main/java/travelbeeee/PDFLO/web/config/WebConfig.java
@@ -1,21 +1,21 @@
-package travelbeeee.PDFLO.web.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import travelbeeee.PDFLO.web.intercepter.LoginIntercepter;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginIntercepter())
-                .order(1)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/", "/error/**", "/member/signUp", "/member/login",
-                        "/member/logout", "/member/sendMail", "/member/auth", "/member/duplicateCheck",
-                        "/js/**", "/css/**", "/img/**")
-                .excludePathPatterns("/test/**", "/test");
-    }
-}
+//package travelbeeee.PDFLO.web.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import travelbeeee.PDFLO.web.intercepter.LoginIntercepter;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new LoginIntercepter())
+//                .order(1)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/", "/error/**", "/member/signUp", "/member/login",
+//                        "/member/logout", "/member/sendMail", "/member/auth", "/member/duplicateCheck", "/images/**",
+//                        "/js/**", "/css/**", "/img/**")
+//                .excludePathPatterns("/test/**", "/test");
+//    }
+//}
