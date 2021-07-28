942ce7f670906fff0d1e517bad32da1637db582a
travelbeeee <sochun1518@naver.com>
2021-07-28 16:35:59 +0900

    [서버] Test코드 주석처리

--- V3.0/PDFLO/src/test/java/travelbeeee/PDFLO/domain/repository/ItemJDBCRepositoryTest.java
+++ V3.0/PDFLO/src/test/java/travelbeeee/PDFLO/domain/repository/ItemJDBCRepositoryTest.java
@@ -1,22 +1,22 @@
-package travelbeeee.PDFLO.domain.repository;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import travelbeeee.PDFLO.domain.model.dto.ItemViewDto;
-
-import java.util.List;
-
-@SpringBootTest
-class ItemJDBCRepositoryTest {
-    @Autowired
-    ItemJDBCRepository itemJDBCRepository;
-
-    @Test
-    void test(){
-        List<ItemViewDto> itemViewDto = itemJDBCRepository.findAllItemViewDtoOrderByPopular();
-        for (ItemViewDto viewDto : itemViewDto) {
-            System.out.println(viewDto);
-        }
-    }
-}
+//package travelbeeee.PDFLO.domain.repository;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import travelbeeee.PDFLO.domain.model.dto.ItemViewDto;
+//
+//import java.util.List;
+//
+//@SpringBootTest
+//class ItemJDBCRepositoryTest {
+//    @Autowired
+//    ItemJDBCRepository itemJDBCRepository;
+//
+//    @Test
+//    void test(){
+//        List<ItemViewDto> itemViewDto = itemJDBCRepository.findAllItemViewDtoOrderByPopular();
+//        for (ItemViewDto viewDto : itemViewDto) {
+//            System.out.println(viewDto);
+//        }
+//    }
+//}
