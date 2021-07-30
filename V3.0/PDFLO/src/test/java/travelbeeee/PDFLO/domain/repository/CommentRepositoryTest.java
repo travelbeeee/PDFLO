0463fa16b689041b8e73e18aa86a1eb3b2f5a0d9
travelbeeee <sochun1518@naver.com>
2021-07-30 11:59:47 +0900

    [test] 답글남기기 테스트

---
+++ V3.0/PDFLO/src/test/java/travelbeeee/PDFLO/domain/repository/CommentRepositoryTest.java
@@ -0,0 +1,34 @@
+package travelbeeee.PDFLO.domain.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import travelbeeee.PDFLO.domain.model.entity.Comment;
+import travelbeeee.PDFLO.domain.model.entity.Item;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CommentRepositoryTest {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    void findAllWithRecommentByItem_테스트(){
+        List<Comment> comments = commentRepository.findAllWithRecommentByItem(5L);
+        for (Comment c : comments) {
+            System.out.println("comment id : " + c.getId());
+            if(c.getRecomment() != null){
+                System.out.println("recomment id : " + c.getRecomment().getId());
+            }
+        }
+    }
+
+}
--- V3.0/PDFLO/src/test/java/travelbeeee/PDFLO/domain/repository/ItemRepositoryTest.java
+++ V3.0/PDFLO/src/test/java/travelbeeee/PDFLO/domain/repository/ItemRepositoryTest.java
@@ -1,9 +1,14 @@
 package travelbeeee.PDFLO.domain.repository;
 
+import org.assertj.core.api.Assert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import travelbeeee.PDFLO.domain.model.entity.Item;
 
 import javax.persistence.EntityManager;
+import java.util.Optional;
 
 @SpringBootTest
 class ItemRepositoryTest {
@@ -11,7 +16,12 @@ class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
-    @Autowired
-    EntityManager em;
+    @Test
+    void ItemDetailDTO테스트(){
+        Optional<Item> findItem = itemRepository.findWithMemberAndPdfAndThumbnailAndCommentAndRecommentById(5L);
+        Assertions.assertThat(findItem.isPresent()).isTrue();
+        Item item = findItem.get();
+        Assertions.assertThat(item.getComments().size()).isEqualTo(1);
+    }
 
 }
