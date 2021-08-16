cce173781b99472ed70cac228accd5dea4dadfe3
travelbeeee <sochun1518@naver.com>
2021-08-16 20:39:31 +0900

    [refactor] 페이징 처리 클래스 내부 로직 수정

--- V3.0/PDFLO/src/main/java/travelbeeee/PDFLO/domain/utility/PageMaker.java
+++ V3.0/PDFLO/src/main/java/travelbeeee/PDFLO/domain/utility/PageMaker.java
@@ -4,33 +4,34 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 
-/**
- * 현재 데이터의 totalPage 수와 pageSize 를 기반으로
- * startPageNum, endPageNum, prevPageNum, nextPageNumn을 계산해 Model에 추가해준다.
- */
+
 @Component
 @Slf4j
 public class PageMaker {
 
-    public static void makePage(Integer totalPages, Integer pageSize, Integer pageNum, Model model) {
+    /**
+     * 현재 totalPage 수와 pageSize 를 기반으로
+     * startPageNum, endPageNum, prevPageNum, nextPageNumn을 계산해 Model에 추가해준다.
+     */
+    public static void calcPageNum(Integer totalPages, Integer pageSize, Integer pageNum, Model model) {
         log.info("PageMaker - makePage");
-        Integer startPageNum = (pageNum / pageSize) * pageSize + 1;
-        Integer endPageNum = (pageNum / pageSize) * pageSize + pageSize;
-        Integer prevPageNum = (pageNum / pageSize) * pageSize + 1 - pageSize;
-        Integer nextPageNum = (pageNum / pageSize) * pageSize + pageSize + 1;
-
         if(totalPages == 0) return;
 
-        if(prevPageNum >= 1){
+        Integer startPageNum = (pageNum / pageSize) * pageSize;
+        Integer endPageNum = (pageNum / pageSize) * pageSize + (pageSize - 1);
+        Integer prevPageNum = startPageNum - pageSize;
+        Integer nextPageNum = endPageNum + 1;
+
+        if(prevPageNum >= 0){
             log.info("prevPageNum : {}", prevPageNum);
             model.addAttribute("prevPageNum", prevPageNum);
         }
-        if (nextPageNum <= totalPages) {
+        if (nextPageNum < totalPages) {
             log.info("nextPageNum : {}", nextPageNum);
             model.addAttribute("nextPageNum", nextPageNum);
         }
         if(endPageNum >= totalPages){
-            endPageNum = totalPages;
+            endPageNum = totalPages - 1;
         }
         model.addAttribute("startPageNum", startPageNum);
         model.addAttribute("endPageNum", endPageNum);
