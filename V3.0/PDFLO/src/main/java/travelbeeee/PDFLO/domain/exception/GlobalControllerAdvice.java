772f46f864c3aec1020bdb98df35a6661a242d4e
travelbeeee <sochun1518@naver.com>
2021-08-18 17:17:08 +0900

    [refactor] 에러 상황 처리 수정

--- V3.0/PDFLO/src/main/java/travelbeeee/PDFLO/domain/exception/GlobalControllerAdvice.java
+++ V3.0/PDFLO/src/main/java/travelbeeee/PDFLO/domain/exception/GlobalControllerAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 
 
@@ -12,41 +13,45 @@ import java.util.HashMap;
 @ControllerAdvice("travelbeeee.PDFLO.web.controller")
 public class GlobalControllerAdvice {
 
-    private HashMap<ReturnCode,String> viewMap = new HashMap<ReturnCode,String>();
+    private HashMap<ReturnCode,String> redirectMap = new HashMap<ReturnCode,String>();
 
-//    @PostConstruct
-//    private void postConstruct(){
-//        viewMap.put(Code.MEMBER_NAME_DUPLICATION, "/member/signUp");
-//        viewMap.put(Code.LOGIN_INPUT_INVALID, "/member/login");
-//        viewMap.put(Code.SIGNUP_INPUT_INVALID, "/member/signUp");
-//        viewMap.put(Code.PASSWORD_INPUT_INVALID, "/member/login");
-//        viewMap.put(Code.COMMENT_NO_PERMISSION_BUYING, "/");
-//        viewMap.put(Code.COMMENT_ALREADY_WRITTEN, "/");
-//        viewMap.put(Code.COMMENT_NO_PERMISSION_WRITER, "/");
-//        viewMap.put(Code.COMMENT_INPUT_INVALID, "/");
-//        viewMap.put(Code.ITEM_ALREADY_BOUGHT, "/");
-//        viewMap.put(Code.MEMBER_INSUFFICIENT_BALANCE, "/");
-//        viewMap.put(Code.MEMBER_NO_PERMISSION, "/member/login");
-//        viewMap.put(Code.MEMBER_IS_SELLER, "/");
-//        viewMap.put(Code.MAIL_AUTHCODE_INCORRECT, "/member/login");
-//        viewMap.put(Code.ITEM_INPUT_ERROR, "/item/upload");
-//        viewMap.put(Code.MEMBER_ALREADY_LOGIN, "/");
-//        viewMap.put(Code.MEMBER_NOT_SELLER, "/");
-//        viewMap.put(Code.DOWNLOAD_NO_PERMISSION, "/");
-//        viewMap.put(Code.MEMBER_NO_EXIST, "/member/signUp");
-//        viewMap.put(Code.ITEM_NO_EXIST, "/");
-//        viewMap.put(Code.PROFILE_NO_EXIST, "/member/mypage");
-//        viewMap.put(Code.COMMENT_NO_EXIST, "/");
-//        viewMap.put(Code.CART_NO_EXIST, "/");
-//        viewMap.put(Code.CART_ALREADY_EXIST, "/");
-//    }
+    @PostConstruct
+    private void postConstruct(){
+        redirectMap.put(ReturnCode.MEMBER_NO_EXIST, "/member/signUp");
+        redirectMap.put(ReturnCode.MEMBER_NOT_SELLER, "/");
+        redirectMap.put(ReturnCode.MEMBER_IS_SELLER, "/");
+        redirectMap.put(ReturnCode.MEMBER_NAME_DUPLICATION, "/member/signUp");
+        redirectMap.put(ReturnCode.MEMBER_INSUFFICIENT_BALANCE, "/member/mypage");
+        redirectMap.put(ReturnCode.MEMBER_NO_PERMISSION, "/member/login");
+        redirectMap.put(ReturnCode.MEMBER_ALREADY_LOGIN, "/");
+        redirectMap.put(ReturnCode.LOGIN_INPUT_INVALID, "/member/login");
+        redirectMap.put(ReturnCode.SIGNUP_INPUT_INVALID, "/member/signUp");
+        redirectMap.put(ReturnCode.PASSWORD_INPUT_INVALID, "/member/login");
+
+        redirectMap.put(ReturnCode.COMMENT_NO_EXIST, "/");
+        redirectMap.put(ReturnCode.COMMENT_NO_PERMISSION_BUYING, "/");
+        redirectMap.put(ReturnCode.COMMENT_ALREADY_WRITTEN, "/");
+        redirectMap.put(ReturnCode.COMMENT_NO_PERMISSION_WRITER, "/");
+        redirectMap.put(ReturnCode.COMMENT_INPUT_INVALID, "/");
+
+        redirectMap.put(ReturnCode.ITEM_NO_EXIST, "/");
+        redirectMap.put(ReturnCode.ITEM_ALREADY_BOUGHT, "/");
+        redirectMap.put(ReturnCode.ITEM_INPUT_ERROR, "/item/upload");
+
+        redirectMap.put(ReturnCode.MAIL_AUTHCODE_INCORRECT, "/member/login");
+        redirectMap.put(ReturnCode.DOWNLOAD_NO_PERMISSION, "/");
+        redirectMap.put(ReturnCode.PROFILE_NO_EXIST, "/member/mypage");
+        redirectMap.put(ReturnCode.CART_NO_EXIST, "/");
+        redirectMap.put(ReturnCode.CART_ALREADY_EXIST, "/");
+    }
 
     @ExceptionHandler({travelbeeee.PDFLO.domain.exception.PDFLOException.class})
     public ModelAndView handleMyException(travelbeeee.PDFLO.domain.exception.PDFLOException e){
         ModelAndView mav = new ModelAndView();
-        String viewName = viewMap.get(e.getReturnCode());
+        String redirectURL = redirectMap.get(e.getReturnCode());
         mav.setViewName("/errors/error");
         mav.addObject("errorCode", e.getReturnCode().getMessage());
+        mav.addObject("redirectURL", redirectURL);
         return mav;
     }
 }
