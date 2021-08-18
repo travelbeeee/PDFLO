6350efdef49ec543683cfb48c236e68760b71a78
travelbeeee <sochun1518@naver.com>
2021-08-18 17:17:32 +0900

    [chore] 주석 수정

--- V3.0/PDFLO/src/main/java/travelbeeee/PDFLO/domain/model/dto/ItemDto.java
+++ V3.0/PDFLO/src/main/java/travelbeeee/PDFLO/domain/model/dto/ItemDto.java
@@ -18,11 +18,6 @@ import java.util.Date;
  * Main 페이지에 뿌리는 ItemDto
  * CommentTable과 ItemTable을 참조해 아이템별로
  * 상품번호, 상품제목, 판매자, 상품등록날짜, 썸네일, 평점, 후기갯수 가져오기위한 Dto
- *
- * select avg(score) as avgScore, count(*) as commentCnt, comment.item_id, item.thumbnail_id, item.title, item.created_date, item.username
- * from comment right outer join item
- * on (comment.item_id= item.item_id)
- * group by (item.item_id);
  */
 @Getter @Setter
 @ToString
